a74ce35a7e2ecc995b3bdf45a59de46762a57db0
Babs-Olokode Aliyah <aliyaheniola91@gmail.com>
2023-10-17 22:51:48 +0100

    initial commit

---
+++ src/pages/authentication/components/SignIn.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {useFormik} from "formik";
+import * as Yup from "yup";
+
+const SignIn = () => {
+
+  return (
+    <div>SignIn</div>
+    
+  )
+}
+
+export default SignIn
---
+++ src/pages/authentication/components/SignUp.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {useFormik} from "formik";
+import * as Yup from "yup";
+
+const SignUp = () => {
+  return (
+    <div>
+        SignUp</div>
+ 
+  )
+}
+
+export default SignUp
